ce53e287a7ad0deafd094eccf27a512a83262b1d
Andrew Plonski <agplonski@hotmail.com>
2012-11-30 18:35:21 -0600

    remove mailer in .rb

--- config/initializers/mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app9595209@heroku.com'],
-  :password       => ENV['2votc5pz'],
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
